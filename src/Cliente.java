948dbedcda68b271c29af856df0805f7a3a8520a
Alan Gael Vilchis Testas (github:alanvilchisssss) <alanvilchis07@gmail.com>
2024-09-16 14:54:07 -0600

    Avance de setters,getters, clase constructor y atributos del cliente

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,19 +1,80 @@
 import java.util.LinkedList;
 import java.util.Random;
 public class Cliente {
-    public int numeroDeCuenta;
-    int[] fechaDeNacimiento = new int[3];
-    double saldodeAhorro;
-    String nombre;
-    String apellido;
-    String direccion;
-    String telefono;
-    String correo;
-    LinkedList<Double>[] tarjetas;
-    public static Cliente cliente(){
+    private int numeroDeCuenta;
+    private String fechaDeNacimiento;
+    private String nombre;
+    private String apellido;
+    private String direccion;
+    private String telefono;
+    private String correo;
+    //LinkedList<Double>[] tarjetas;
+    //Constructor
+    public Cliente(int numeroDeCuenta, String fechaDeNacimiento, String nombre, String apellido, String Direccion, String telefono, String correo){
+        this.numeroDeCuenta=numeroDeCuenta;
+        this.fechaDeNacimiento=fechaDeNacimiento;
+        this.nombre=nombre;
+        this.apellido=apellido;
+        this.direccion=Direccion;
+        this.telefono=telefono;
+        this.correo=correo;
+    }
+    //Getters:
+    public int getnumeroDecuenta(){
+        return numeroDeCuenta;
+    }
+    public String getfechaDeNacimiento(){
+        return fechaDeNacimiento;
+    }
+    public String getnombre(){
+        return nombre;
+    }
+    public String getapellidos(){
+        return apellido;
+    }
+    public String getdireccion(){
+        return direccion;
+    }
+    public String getelefono(){
+        return telefono;
+    }
+    public String getcorreo(){
+        return telefono;
+    }
+    //setters:
+    public void setnumeroDecuenta(int numeroDeCuenta){
+        if(numeroDeCuenta>0){
+            this.numeroDeCuenta=numeroDeCuenta;
+        }else{
+            System.out.println("Dato no válido.");
+        }
+    }
+    public void setfechaDeNacimiento(String Fecha){
+        if(Fecha.length()==10){
+        this.fechaDeNacimiento=Fecha;
+        }else if(Fecha.isBlank()||Fecha.length()<10){
+            System.out.println("Intente Otra vez");
+        }
+    }
+    public void setnombre(String nombre){
+        this.nombre=nombre;
+    }
+    public void setapellidos(String Apellidos){
+        this.apellido=Apellidos;
+    }
+    public void setdireccion(String Direccion){
+        this.direccion=Direccion;
+    }
+    public void settelefono(String telefono){
+        this.telefono=telefono;
+    }
+    public void setcorreo(String correo){
+        this.correo=correo;
+    }
+    /*public static Cliente cliente(){
         Random random = new Random();
         int numeroDeCuenta=random.nextInt(500);
         return new Cliente();
-    }
+    }*/
 }
 
